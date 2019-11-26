6096de3faa9ac0d02104138d1821f35e23f5f9bb
Aiyeola Victor <aiyeolavictor@gmail.com>
2019-11-26 14:39:35 +0100

    Complete rendering of template with relevant props

--- src/Ninjas.js
+++ src/Ninjas.js
@@ -1,16 +1,31 @@
 import React from "react";
-
-const Ninjas = ({ ninjas }) => {
+// This is a UI component
+const Ninjas = ({ ninjas , deleteNinja }) => {
+  // CONDITIONAL OUTPUTS 
+//   const ninjaList = ninjas.map(ninja => {
+//     if (ninja.age > 20) {
+//       return (
+//         <div className="ninja" key={ninja.id}>
+//           <div>Name: {ninja.name}</div>
+//           <div>Age: {ninja.age}</div>
+//           <div>Belt: {ninja.belt}</div>
+//         </div>
+//       );
+//     } else return null;
+//   });
+// }; 
+  // Second Conditional Statement
   const ninjaList = ninjas.map(ninja => {
-    return (
+    return ninja.age > 20 ? (
       <div className="ninja" key={ninja.id}>
         <div>Name: {ninja.name}</div>
         <div>Age: {ninja.age}</div>
         <div>Belt: {ninja.belt}</div>
+        <button onClick={() => {deleteNinja(ninja.id)}}>Delete ninja </button>
       </div>
-    );
+    ) : null;
   });
   return <div className="ninja-list">{ninjaList}</div>;
-};
+}
 
 export default Ninjas;
